0427b07cfe9934c91997becbb3d1fcb034e42c31
kaartiiik <kgupta@trueigtech.com>
2026-06-20 21:57:42 +0530

    fix navbar casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Products from "./pages/Products";
 import Customers from "./pages/Customers";
 import Orders from "./pages/Orders";
